6c58ceae948ab8788393c5e58d64c720966f27f6
dinaKSari <dinakumala7070@gmail.com>
2026-03-08 15:19:14 +0700

    membuat resouce post dan implementasi form element

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DatePicker;
+
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make('title')->required(),
+                TextInput::make('slug')->required(),
+                Select::make("category_id")
+                    ->relationship("category", "name")
+                    ->preload()
+                    ->searchable(),
+                ColorPicker::make('color'),
+                MarkdownEditor::make('body'),
+                //alternatif Markdown
+                //RichEditor::make('body'),
+                FileUpload::make("Image")
+                    ->disk("public")
+                    ->directory("posts"),
+                TagsInput::make('tags'),
+                Checkbox::make('published'),
+                DatePicker::make('published_at'),
+            ]);
+    }
+}
