f4eefda7d0c108398fc90092788678eee0f27ea7
dinaKSari <dinakumala7070@gmail.com>
2026-03-10 01:26:28 +0700

    membuat agar tombol default dihilangkan dan agar bisa liat list di table

--- app/Filament/Resources/Products/Pages/CreateProduct.php
+++ app/Filament/Resources/Products/Pages/CreateProduct.php
@@ -8,4 +8,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+    protected function getFormActions(): array
+    {
+        return [];
+    }
 }
