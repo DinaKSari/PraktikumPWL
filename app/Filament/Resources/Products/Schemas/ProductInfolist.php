d19dc74c7206a1111b4ef9c42447faf6ca702ed4
dinaKSari <dinakumala7070@gmail.com>
2026-03-10 01:53:44 +0700

    membuat 3 section untuk produk, price, image

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Products\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
 
 class ProductInfolist
 {
@@ -11,6 +14,49 @@ class ProductInfolist
         return $schema
             ->components([
                 //
+                Section::make('Product Info')
+                    ->description('')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('id')
+                         ->label('Product ID'),
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('success'),
+                        TextEntry::make('description')
+                            ->label('Product Description'),
+                    ])
+                ->columnSpanFull(),
+                Section::make('Pricing & Stock')
+                    ->schema([
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->icon('heroicon-o-currency-dollar'),
+                        TextEntry::make('stock')
+                         ->label('Product Stock'),
+                    ])
+                ->columnSpanFull(),
+                Section::make ('Image and Status')
+                    ->description('')
+                    ->schema([
+                        ImageEntry::make('image')
+                            ->label('Product Image')
+                            ->disk('public'),
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-s-currency-dollar'),
+                        TextEntry::make('stock')
+                            ->label('Product Stock')
+                            ->weight('bold')
+                            ->color('primary'),
+                    ])
+                ->columnSpanFull(),
             ]);
     }
 }
