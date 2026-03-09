b3349126845b6fa616aaa3ed0163eb4aaf1a991f
dinaKSari <dinakumala7070@gmail.com>
2026-03-10 01:10:56 +0700

    menambahkan step step

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -8,6 +8,9 @@ use Filament\Schemas\Components\Wizard\Step;
 use Filament\Schemas\Components\Group;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Checkbox;
+use Filament\Actions\Action;
 
 class ProductForm
 {
@@ -40,8 +43,24 @@ class ProductForm
                             ])->columns(2),
                             MarkdownEditor:: make('description')
                         ]),
+                // step media status
+                    Step::make('Media & Status')
+                        ->description('Upload gambar dan atur status')
+                        ->schema([
+                            FileUpload::make('image')
+                            ->disk('public')
+                            ->directory('products'),
+                            Checkbox::make('is_active'),
+                            Checkbox::make('is_featured'),
+                        ]),
                 ])
-                ->columnSpanFull(),
+                ->columnSpanFull()
+                ->submitAction(
+                    Action::make('save')
+                    ->label('Save Product')
+                    ->color('primary')
+                    ->submit('save')
+                )
             ]);
     }
 }
